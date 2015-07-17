8238075a704737b013804cc511d55db04821fa55
ryanda <ryandaputra@gmail.com>
2015-07-18 00:30:24 +0700

    dunno

--- login/index.php
+++ login/index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 include "koneksi.php";
 if (isset($_POST['login'])) {
@@ -97,4 +98,104 @@ unset($_POST['login']);
         <script src="../asset/js/jquery.min.js"></script>
 	    <script src="../asset/js/materialize.min.js"></script>
     </body>
+=======
+<?php
+include "koneksi.php";
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $query = mysql_query("Select * from user where username='$username' and password='$password'");
+    $cekdata = mysql_num_rows($query);
+    if ($cekdata > 0) {
+        $data = mysql_fetch_array($query);
+        $level = $data['level'];
+        echo $level;
+        if ($level == "1") {
+?><script language="javascript">document.location.href="../admin/beranda/menu.php";</script><?php
+}
+else if ($level == "2") {
+?><script language="javascript">document.location.href="../pegawai/index.php";</script><?php
+}
+else {
+?><script language="javascript">document.location.href="../kepala/index.php";</script><?php
+}
+}
+else {
+header('Location:index.php?login=Maaf password atau username anda salah!');
+}
+}
+else {
+unset($_POST['login']);
+}
+?>
+
+<html>
+    <head>
+        <title>Login</title>
+	    <link href="../asset/css/materialize.min.css" rel="stylesheet">
+	    <style type="text/css">
+		    body {
+	            display: flex;
+	            min-height: 100vh;
+	            flex-direction: column;
+	        }
+	        main {
+	            flex: 1 0 auto;
+	        }
+        </style>
+    </head>
+    <body>
+    	<!--    header navbar-->
+	    <header>
+	        <nav class="green darken-4">
+	            <div class="nav-wrapper container">
+	                <a class="brand-logo">Silahkan Login</a>
+	            </div>
+	        </nav>
+	    </header>
+	    <!--    end header navbar-->
+
+	    <!--    main content-->
+	    <main style="margin-top:40px;">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col s5 offset-s3">
+	                <!-- tanda aja -->
+	                	<form action="index.php" method="post" name="postform">
+	                		<p>Username</p>
+			                <input type="text" name="username"/></br>
+			                <p>Password</p>
+			                <input type="password"  name="password" /></br>
+			                <button class="btn waves-effect waves-light" type="submit" name="login">Login
+						    	<i class="mdi-content-send right"></i>
+						  	</button>
+							</br>
+							</br>
+			                <?php
+				            if (isset($_GET['login'])) {
+				                echo $_GET['login'];
+				            } ?>
+		                </form>
+	                </div>
+	            </div>
+	        </div>
+	    </main>
+	    <!--    end main content-->
+
+	    <!--    footer page-->
+	    <footer class="page-footer green darken-4">
+	        <div class="container">
+                <h5 class="white-text">Knowledge Management System</h5>
+		        <div class="footer-copyright">
+		            © 2015 Copyright miftaamelia
+		        </div>
+	        </div>
+	    </footer>
+	    <!--    end footer page-->
+
+        <script src="../asset/js/jquery.min.js"></script>
+	    <script src="../asset/js/materialize.min.js"></script>
+    </body>
+>>>>>>> Stashed changes
 </html>
