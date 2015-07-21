7f7830b827268350578ef701afec6ac0db886b87
andrietri <andrietrilaksono@mhs.uinjkt.ac.id>
2015-07-22 00:53:13 +0700

    admin module menu_utama tampil.php complete(y)

--- admin/module/menu_utama/tampil.php
+++ admin/module/menu_utama/tampil.php
@@ -1,46 +1,59 @@
 <?php
-
 require "includes/header_mu.php";
 require "includes/config.php";
 ?>
+
 <body>
-		
-		<div class="container">
-			<div class="hero-unit">	
-<table class="table table-striped" border="0">
-				<thead>
-<tr>
-  <th scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>No</th>
-  <th scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Menu Utama</th>
-  <th scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Link</th>
-  <th scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Aksi</th>
-</tr>
-</thead>
+	<main style="margin-top:30px; margin-bottom:30px;">	
+	<div class="container">
+	<table class="hoverable centered responsive-table">
+	<thead>
+		<tr>
+  		<th>No</th>
+  		<th>Menu Utama</th>
+  		<th>Link</th>
+	  	<th>Aksi</th>
+		</tr>
+	</thead>
+
 <?php
 $q=mysql_query("SELECT * FROM menu_utama");
 $no=1;
 while($r=mysql_fetch_array($q)){
 ?>
+
 <tbody>
 <tr>
-<td><center><?php echo $no; ?></td>
-<td><center><?php echo $r['nama_menu']; ?></td>
-<td><center><?php echo $r['link']; ?></td>
-<td><center><a id="edit" href="edit.php?id_menu=<?php echo $r['id_menu'] ?>">Ubah</a></td>
+<td><?php echo $no; ?></td>
+<td><?php echo $r['nama_menu']; ?></td>
+<td><?php echo $r['link']; ?></td>
+<td><a id="edit" href="edit.php?id_menu=<?php echo $r['id_menu'] ?>">Ubah</a></td>
 </tr>
 </tbody>
+
 <?php
 $no++;
 }
 ?>
+
 </table>
 </br>
 </br>
 
-<?php echo "Data pada Menu Utama tidak dapat dihapus, tetapi dapat di non-aktifkan melalui ubah menu utama";?>
-</table>
+<div class="row">
+      <div class="col s12 m5">
+        <div class="card-panel green">
+          <span class="white-text">
+          <?php echo "Data pada Menu Utama tidak dapat dihapus, tetapi dapat di non-aktifkan melalui ubah menu utama";?>
+          </span>
+        </div>
+      </div>
+    </div>
+
+
 			</div>
 		</div>
+	</main>
 </body>	
 
 
