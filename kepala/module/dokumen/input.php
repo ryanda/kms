56ff25e011607966488d64c3048e7f64cf1e6692
ryanda <ryandaputra@gmail.com>
2015-07-21 23:04:44 +0700

    kepala module dokumen fix (y)

--- kepala/module/dokumen/input.php
+++ kepala/module/dokumen/input.php
@@ -4,76 +4,22 @@ require "includes/header_artikel_input.php";
 require "includes/config.php";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!-- TinyMCE -->
-<script type="text/javascript" src="includes/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
-<script type="text/javascript">
-tinyMCE.init({
-        // General options
-        mode : "textareas",
-        theme : "advanced",
-        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-
-        // Theme options
-        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : "bottom",
-        theme_advanced_resizing : true,
-
-        // Skin options
-        skin : "o2k7",
-        skin_variant : "black",
-
-        // Example content CSS (should be your site CSS)
-        content_css : "css/example.css",
-
-        // Drop lists for link/image/media/template dialogs
-        template_external_list_url : "js/template_list.js",
-        external_link_list_url : "js/link_list.js",
-        external_image_list_url : "js/image_list.js",
-        media_external_list_url : "js/media_list.js",
-
-        // Replace values for the template plugin
-        template_replace_values : {
-                username : "Some User",
-                staffid : "991234"
-        }
-});
-</script>
-</head>
-<body>
-		<div class="container">
-			<div class="hero-unit">
-			<?
-				
-			?>
-				
-				<form name="input_data" action="proses.php" method="post">
-					<table class="none">	 
-						<thead>	
-							<tr>
-								<td></td>
-								<td><input name="judul" type="text" size="40"  placeholder="Judul" required autofocus/></td>
-							</tr>
-							<tr>
-								<td>&nbsp;</td>
-								<td><textarea id="elm1" name="isi" rows="15" cols="60" ></textarea></td>
-							</tr>
-							<tr>
-								<td>&nbsp;</td>
-								<td><input type="submit" name="proses" value="Post" class="btn btn-inverse"/>
-								<input type="reset" name="reset" value="Reset" class="btn btn-inverse"></td>
-							</tr>
-						</thead>
-					</table>
-				</form>
-			</div>
-		</div>
+<main style="margin-top:30px;">
+        <div class="container row">
+                <form name="input_data" action="proses.php" method="post">
+                        <div class="input-field col s12">
+                                <input type="text" id="judul" name="judul" required>
+                                <label for="judul">Judul Dokumen</label>
+                        </div>
+                        <div class="input-field col s12">
+                                <textarea id="isi" name="isi" rows="9" cols="60" class="materialize-textarea" required></textarea>
+                                <label for="isi">Isi Dokumen</label>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="proses">
+                                Submit <i class="mdi-content-send right"></i>
+                        </button>
+                </form>
+        </div>
+</main>
 </body>
 </html>
--- kepala/module/dokumen/tampil.php
+++ kepala/module/dokumen/tampil.php
@@ -1,49 +1,51 @@
 <?php
-
 require "includes/header_artikel.php";
 require "includes/config.php";
 ?>
 <body>
-		
-		<div class="container">
-			<div class="hero-unit">	
-				<table class="table table-striped" border="0">
-				<thead>
-				<tr>
-					<th width="10" scope scope="col" bgcolor="#333333"><font color="#CCCCCC">No</th>
-					<th width="100" scope scope="col" bgcolor="#333333"><font color="#CCCCCC">Judul</th>
-                    <th width="500" scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Isi</th>
-					<th scope scope="col" bgcolor="#333333"><font color="#CCCCCC">Tanggal</th>
-					<th scope scope="col" bgcolor="#333333"><font color="#CCCCCC">Waktu</th>
-					<th width="50" scope scope="col" bgcolor="#333333"><font color="#CCCCCC">Pembaca</th>
-					<th width="120" scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Opsi</th>
-				</thead>
-				</tr>
-				
-	<?php
-	$no = 1;
-	$query2 = "SELECT * from dokumen ";
-	$sql = mysql_query ($query2);
-	while ($hasil = mysql_fetch_array ($sql)) {
-			?>
-				<tbody>
-				<tr>
-					<td align="center"><?php echo $no?></td>
+		 <!-- start main content -->
+        <main style="margin-top:30px; margin-bottom:30px;">
+        <div class="container">
+        <table class="hoverable centered responsive-table">
+          <thead>
+              <tr>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Isi</th>
+                <th>Tanggal</th>
+                <th>Waktu</th>
+                <th>Pembaca</th>
+              </tr>
+          </thead>
+
+			<?php
+			$no = 1;
+			$query2 = "SELECT * from dokumen ";
+			$sql = mysql_query ($query2);
+			while ($hasil = mysql_fetch_array ($sql)) {
+			?>		
+
+             <tbody> 
+             
+             <tr>
+
+                    <td><?php echo $no?></td>
 					
 					<td><?php echo $hasil['judul']?></td>
                     <td><?php echo $hasil['isi']?></td>
-					<td align="center"><small><?php echo $hasil['tanggal']?></small></td>
-					<td align="center"><small><?php echo $hasil['waktu']?></small></td>
-					<td align="center"><small><?php echo $hasil['baca']?></small></td> 
-					<td align="center">&nbsp
-					<a href="view.php?id=<?php echo $hasil ['id'];?>">View</a> &nbsp;|
-					<a href="download.php?id=<?php echo $hasil ['id'];?>">Download</a></td>
-				</tr>	
-				</tbody>
-				<?php $no++; ?>
-				<?php } ?>
-				
-				</table>
-			</div>
-		</div>
+					<td><?php echo $hasil['tanggal']?></td>
+					<td><?php echo $hasil['waktu']?></td>
+					<td><?php echo $hasil['baca']?></td> 
+             </tr>
+             </tbody>
+
+             <?php $no++; ?>
+			 <?php } ?>
+
+          </table>
+        </div>
+    </main>
+    <!-- end main content -->
+
 </body>	
+</html>
