1ec91109e92b11d1c4896a0e8c49cf7abcd20037
ryanda <ryandaputra@gmail.com>
2015-07-21 22:44:51 +0700

    admin module dokumen clear (y)

--- admin/module/dokumen/edit.php
+++ admin/module/dokumen/edit.php
@@ -8,69 +8,24 @@ $sql = mysql_query ($query);
 $hasil = mysql_fetch_array ($sql);
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!-- TinyMCE -->
-<script type="text/javascript" src="../../../admin/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
-<script type="text/javascript">
-tinyMCE.init({
-        // General options
-        mode : "textareas",
-        theme : "advanced",
-        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
-        // Theme options
-        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : "bottom",
-        theme_advanced_resizing : true,
-
-        // Skin options
-        skin : "o2k7",
-        skin_variant : "black",
-
-        // Example content CSS (should be your site CSS)
-        content_css : "css/example.css",
-
-        // Drop lists for link/image/media/template dialogs
-        template_external_list_url : "js/template_list.js",
-        external_link_list_url : "js/link_list.js",
-        external_image_list_url : "js/image_list.js",
-        media_external_list_url : "js/media_list.js",
-
-        // Replace values for the template plugin
-        template_replace_values : {
-                
-        }
-});
-</script>
-</head>
-<body>
-		<div class="container">
-
+    <main style="margin-top:30px;">
+        <div class="container row">
 			<form action="simpan.php" method="POST" name="input" >
-				<table class="hoverable responsive-table">
-					
-                    <tr>
-						
-                        <td></td>
-						<td><input name="judul" type="text" value="<?php echo $hasil['judul'];?>"/></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td><textarea id="elm1" name="isi" rows="50" cols="50"><?php echo $hasil['isi'];?></textarea></td>
-					</tr>
-				</table>
-                        
-                        <button class="btn waves-effect waves-light" type="submit" name="simpan">Submit
-                        <i class="mdi-content-send right"></i>
-                        </button>
-
+                <div class="input-field col s12">
+                    <input type="hidden" name="id" value="<?php echo $hasil['id'];?>">
+                    <input type="text" id="judul" name="judul" value="<?php echo $hasil['judul'];?>">
+                    <label for="judul">Judul Dokumen</label>
+                </div>
+                <div class="input-field col s12">
+                    <textarea id="isi" name="isi" rows="9" cols="60" class="materialize-textarea"><?php echo $hasil['isi'];?></textarea>
+                    <label for="isi">Isi Dokumen</label>
+                </div>
+                <button class="btn waves-effect waves-light" type="submit" name="proses">
+                    Submit <i class="mdi-content-send right"></i>
+                </button>
 			</form>
-			
 		</div>
+    </main>
+</body>
+</html>    
