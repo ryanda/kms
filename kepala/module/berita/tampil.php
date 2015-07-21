669a66980ff8d68a9bfb8134e8e9c3abad0d70d7
ryanda <ryandaputra@gmail.com>
2015-07-21 22:56:32 +0700

    kepala module berita fix (y)

--- kepala/module/berita/tampil.php
+++ kepala/module/berita/tampil.php
@@ -2,39 +2,37 @@
 
 require "includes/header_berita.php";
 require "includes/config.php";
-?>
-<body>
-		
+?> 
+
+	<main style="margin-top:30px; margin-bottom:30px;">
 		<div class="container">
-			<div class="hero-unit">	
-				<table class="table table-striped" border="0">
+			<table class="hoverable centered responsive-table">
 				<thead>
-				<tr>
-					<th width="10" scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>No</th></center>
-					<th width="100"scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Judul</th></center>
-                    <th width="200" scope scope="col" bgcolor="#333333"><font color="#CCCCCC">Isi<center></th>
-					<th width="100"scope scope="col" bgcolor="#333333"><font color="#CCCCCC"><center>Tanggal</th></center>
-				</tr>
+					<tr>
+						<th>No</th>
+						<th>Judul</th>
+	                    <th>Isi</th>
+						<th>Tanggal</th>
+					</tr>
 				</thead>
 	<?php
-	$no = 1;
-	$query2 = "SELECT * from berita ";
-	$sql = mysql_query ($query2);
-	while ($hasil = mysql_fetch_array ($sql)) {
-			?>
+		$no = 1;
+		$query2 = "SELECT * from berita";
+		$sql = mysql_query ($query2);
+		while ($hasil = mysql_fetch_array ($sql)) {
+	?>
 				<tbody>
-				<tr>
-					<td><center><small><?php echo $no?></small></td></center>
-					
-					<td><center><small><?php echo $hasil['judul_berita']?></small></td> </center>
-					<td><center><?php echo $hasil['isi']?></center></td> 
-					<td><center><small><?php echo $hasil['tanggal']?></small></td></center>
-				</tr>	
+					<tr>
+						<td><?php echo $no ?></td>
+						<td><?php echo $hasil['judul_berita']?></td>
+						<td><?php echo $hasil['isi']?></td> 
+						<td><?php echo $hasil['tgl_posting_berita']?></td>
+					</tr>	
 				</tbody>
-				<?php $no++; ?>
-				<?php } ?>
-				
-				</table>
-			</div>
+<?php $no++; ?>
+<?php } ?>
+			</table>
 		</div>
+	</main>
 </body>	
+</html>
