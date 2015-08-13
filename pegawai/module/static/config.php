29e85d4612fad05d7b342da0d6036d4f0f536e33
andrietri <andrietrilaksono@mhs.uinjkt.ac.id>
2015-08-13 16:48:24 +0700

    refix header location

--- pegawai/module/static/config.php
+++ pegawai/module/static/config.php
@@ -3,11 +3,11 @@
 
 	if(!isset($_SESSION['username'])){
 		$_SESSION['error'] = 'Anda belum login';
-		header('Location:/login/index.php');
+		header('Location:../login/index.php');
 	} 
 	if($_SESSION['level']!='2'){
 		$_SESSION['error'] = 'Anda tidak memiliki akses Pegawai';
-		header('Location:/login/index.php');
+		header('Location:../login/index.php');
 	}  
   mysql_connect("localhost","root","") or die("Koneksi Gagal");
   mysql_select_db("kms") or die("Database Tidak Ditemukan");
